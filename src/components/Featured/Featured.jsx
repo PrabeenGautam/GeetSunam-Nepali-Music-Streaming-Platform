f9359da1bd7297ede6843f06240ec5bb73024cef
PrabinGautam <prabeen122@gmail.com>
2023-01-26 18:29:34 +0545

    Custom to Swiper Components

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useRef } from "react";
 import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { useMutation, useQueryClient } from "react-query";
@@ -11,11 +11,13 @@ import PauseSong from "../Player/PauseSong";
 import { toggleSongsFavourite } from "@/services/musicApi/postSongs.api";
 import ActionCreators from "@/react-mui-player/redux/actionCreators";
 import { useTranslation } from "react-i18next";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
 
 function Featured({ data: featuredSongs, showSearchBar = false }) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const songsDetails = featuredSongs[currentIndex];
   const currentSong = useSelector((state) => state);
+  const swiperRef = useRef();
 
   const dispatch = useDispatch();
   const queryClient = useQueryClient();
@@ -68,34 +70,6 @@ function Featured({ data: featuredSongs, showSearchBar = false }) {
     zIndex: 999,
   };
 
-  const fadeAnimation = (image) => {
-    image[0].classList.remove("fade");
-    void image[0].offsetWidth;
-    image[0].classList.add("fade");
-  };
-
-  const goToPrevious = () => {
-    const isFirstIndex = currentIndex === 0;
-    const newIndex = isFirstIndex ? featuredSongs.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-    fadeAnimation(document.getElementsByClassName("featured-img"));
-  };
-
-  const goToNext = useCallback(() => {
-    const isLastIndex = currentIndex === featuredSongs.length - 1;
-    const newIndex = isLastIndex ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-    fadeAnimation(document.getElementsByClassName("featured-img"));
-  }, [currentIndex, featuredSongs.length]);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      goToNext();
-    }, 8000);
-
-    return () => clearInterval(timer);
-  }, [currentIndex, goToNext]);
-
   const musicList =
     featuredSongs &&
     featuredSongs.map(({ trackDetails }) => ({
@@ -114,16 +88,31 @@ function Featured({ data: featuredSongs, showSearchBar = false }) {
   };
 
   return (
-    <>
-      <div className="featured">
+    <div className="featured">
+      <Swiper
+        spaceBetween={20}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        speed={800}
+        loop={true}
+        modules={[Autoplay]}
+        pagination={{ el: ".carosel", clickable: true }}
+        onSlideChange={() => {
+          setCurrentIndex(swiperRef.current.realIndex);
+        }}
+        onBeforeInit={(swiper) => {
+          swiperRef.current = swiper;
+        }}>
         <div style={buttonContainer}>
           <div
             className="index-button"
             title="Go to Previous"
-            onClick={goToPrevious}>
+            onClick={() => swiperRef.current?.slidePrev()}>
             <BiLeftArrow />
           </div>
-          <div className="index-button" title="Go to Next" onClick={goToNext}>
+          <div
+            className="index-button"
+            title="Go to Next"
+            onClick={() => swiperRef.current?.slideNext()}>
             <BiRightArrow />
           </div>
         </div>
@@ -132,78 +121,72 @@ function Featured({ data: featuredSongs, showSearchBar = false }) {
             <SearchBar />
           </div>
         )}
-
-        <div className="image-section img">
-          <img
-            src={songsDetails.trackDetails.coverArt}
-            alt="cover"
-            className="featured-img "
-          />
-        </div>
-
-        <div className="details">
-          <div className="title">{t("featuredSongs")}</div>
-          <div className="song-details">
-            <div className="artists">{songsDetails.artists.fullname}</div>
-            <div className="song-name">{songsDetails.trackDetails.title}</div>
-          </div>
-          {featuredSongs.map((value, index) => {
-            return (
-              <div
-                className={`play-featured ${
-                  index === currentIndex ? "" : "hidden"
-                }`}
-                key={index}>
-                {currentSong.trackID === value._id &&
-                currentSong.mediaState === "PLAYING" ? (
-                  <PauseSong trackDetails={value.trackDetails}>
-                    <Btn className="btn-pause">{t("pause")}</Btn>
-                  </PauseSong>
-                ) : (
-                  <PlaySong
-                    trackDetails={value.trackDetails}
-                    musicList={musicList}>
-                    <Btn className="btn-play">{t("play")}</Btn>
-                  </PlaySong>
-                )}
-
-                <span onClick={(e) => handleFavourite(e, value)}>
-                  {currentSong.trackID === value._id &&
-                    (currentSong.favourite ? (
-                      <AiFillHeart className="featured-heart" />
-                    ) : (
-                      <AiOutlineHeart className="featured-heart" />
-                    ))}
-
-                  {currentSong.trackID !== value._id &&
-                    (value.trackDetails.isFavourite ? (
-                      <AiFillHeart className="featured-heart" />
-                    ) : (
-                      <AiOutlineHeart className="featured-heart" />
-                    ))}
-                </span>
+        {featuredSongs.map((value, index) => {
+          return (
+            <SwiperSlide key={value._id}>
+              <div className="image-section img">
+                <img
+                  src={value.trackDetails.coverArt}
+                  alt="cover"
+                  className="featured-img "
+                />
               </div>
-            );
-          })}
-        </div>
 
+              <div className="details">
+                <div className="title">{t("featuredSongs")}</div>
+                <div className="song-details">
+                  <div className="artists">{value.artists.fullname}</div>
+                  <div className="song-name">{value.trackDetails.title}</div>
+                </div>
+
+                <div className={`play-featured`} key={index}>
+                  {currentSong.trackID === value._id &&
+                  currentSong.mediaState === "PLAYING" ? (
+                    <PauseSong trackDetails={value.trackDetails}>
+                      <Btn className="btn-pause">{t("pause")}</Btn>
+                    </PauseSong>
+                  ) : (
+                    <PlaySong
+                      trackDetails={value.trackDetails}
+                      musicList={musicList}>
+                      <Btn className="btn-play">{t("play")}</Btn>
+                    </PlaySong>
+                  )}
+
+                  <span onClick={(e) => handleFavourite(e, value)}>
+                    {currentSong.trackID === value._id &&
+                      (currentSong.favourite ? (
+                        <AiFillHeart className="featured-heart" />
+                      ) : (
+                        <AiOutlineHeart className="featured-heart" />
+                      ))}
+
+                    {currentSong.trackID !== value._id &&
+                      (value.trackDetails.isFavourite ? (
+                        <AiFillHeart className="featured-heart" />
+                      ) : (
+                        <AiOutlineHeart className="featured-heart" />
+                      ))}
+                  </span>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
         <div className="carosel">
           {featuredSongs.map((_, index) => {
             return (
               <div
                 key={index}
                 onClick={() => {
-                  setCurrentIndex(index);
-                  fadeAnimation(
-                    document.getElementsByClassName("featured-img")
-                  );
+                  swiperRef.current.slideTo(index);
                 }}
                 className={currentIndex === index ? "active" : ""}></div>
             );
           })}
         </div>
-      </div>
-    </>
+      </Swiper>
+    </div>
   );
 }
 
