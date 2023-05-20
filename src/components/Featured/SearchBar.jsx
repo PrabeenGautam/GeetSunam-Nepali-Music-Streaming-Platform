aac534113b0ca0430d51ee8068cac4a61ad120c5
PrabinGautam <prabeen122@gmail.com>
2023-05-20 18:01:16 +0545

    Added debounce search

--- src/components/Featured/SearchBar.jsx
+++ src/components/Featured/SearchBar.jsx
@@ -1,3 +1,5 @@
+import useDebounce from "@/hooks/useDebounce";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FiMic, FiSearch } from "react-icons/fi";
 import { useNavigate, createSearchParams } from "react-router-dom";
@@ -6,19 +8,21 @@ function SearchBar() {
   const navigate = useNavigate();
   const { t } = useTranslation("translation", { keyPrefix: "placeholder" });
 
-  const onSubmitValue = (e) => {
-    e.preventDefault();
-    const query = e.target.value;
+  const [query, setQuery] = useState("");
 
-    const options = {
-      pathname: "/results",
-      search: `?${createSearchParams({ query })}`,
-    };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (query) {
+        const options = {
+          pathname: "/results",
+          search: `?${createSearchParams({ query })}`,
+        };
 
-    if (query) {
-      navigate(options, { replace: true });
-    }
-  };
+        navigate(options, { replace: true });
+      }
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [query]);
 
   return (
     // <form className="search-bar" onSubmit={onSubmitValue}>
@@ -29,7 +33,7 @@ function SearchBar() {
         className="text-input"
         placeholder={t("searchSongArtists")}
         name="query"
-        onChange={onSubmitValue}
+        onChange={(e) => setQuery(e.target.value)}
       />
       <input type="submit" hidden />
       {/* <div style={{ borderLeft: "2px solid black", paddingRight: 5 }}></div>
